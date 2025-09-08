abb0dd366909d3f6f7bbc963e93cc5951112ddf6
neo-dev <sei133dirsoa@gmail.com>
2025-09-08 20:45:47 -0300

    Create exercicio6.c

---
+++ Exercícios/lista1/exercicio6.c
@@ -0,0 +1,8 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// objetivo: receber o salário de um funcionário, calcular o valor total a
+// receber considerando uma gratificação de 500 reais e o pagamento de uma taxa
+// equivalente a 10% do salário total.
