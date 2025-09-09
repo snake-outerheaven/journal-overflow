7cf6901e1b6cc8813b3a7d619fe9e5fb5f5d7d50
neo-dev <sei133dirsoa@gmail.com>
2025-09-08 21:10:50 -0300

    Exercicio 6 pronto. ( a refatorar, vou terminar a lista primeiro e depois colocar mais sauce nos códigos.)

--- Exercícios/lista1/exercicio6.c
+++ Exercícios/lista1/exercicio6.c
@@ -1,8 +1,34 @@
-#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 // objetivo: receber o salário de um funcionário, calcular o valor total a
 // receber considerando uma gratificação de 500 reais e o pagamento de uma taxa
 // equivalente a 10% do salário total.
+
+int main(void) {
+  char numbuf[10];
+  float salario;
+  while (1) {
+    printf("\n");
+    printf("Digite o salário do funcionario: ");
+    fgets(numbuf, sizeof(numbuf), stdin);
+
+    if (sscanf(numbuf, "%f", &salario) != 1) {
+      printf("Entrada inválida\n");
+      continue;
+    }
+
+    if (salario <= 0) {
+      printf("Salario inválido\n");
+      continue;
+    }
+
+    salario += 500.0F;
+    salario -= salario * 0.1F;
+
+    printf("O valor total a receber eh: %.2f\n", salario);
+    break;
+  }
+  return 0;
+}
+
+// bem simples, depois vou refatorar para ser um sistema mesmo.
