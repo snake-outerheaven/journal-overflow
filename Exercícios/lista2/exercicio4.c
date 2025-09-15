315454e4715d534aa4b64903121abe8d44333de6
neo-dev <sei133dirsoa@gmail.com>
2025-09-14 23:08:39 -0300

    Exercicio 4 da lista 2 concluído.

--- Exercícios/lista2/exercicio4.c
+++ Exercícios/lista2/exercicio4.c
@@ -1,5 +1,4 @@
 #include <ctype.h>
-#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
@@ -15,10 +14,74 @@
 bool convert_float(char *in, float *out);
 void trim(char *buf);
 void wait(unsigned short ms);
+int check_triangle(float *a, float *b, float *c);
 
 int main(void) {
   char buf[10];
   float lados[3];
+  int result;
+
+  wait(500);
+  puts("Olá usuário, seja bem vindo! O objetivo deste programa é avaliar se os "
+       "3 números reais que você digitar podem formar um triângulo, e se o "
+       "triângulo for possível, haverá uma afirmativa, e logo após será "
+       "informado o tipo deste triângulo");
+  wait(250);
+  puts("Dito isso, vamos começar.");
+
+  while (true) {
+    for (int i = 0; i < 3; i++) {
+      while (true) {
+        printf(
+            "Por favor, digite o %dº lado (até 10 caracteres e ponto ao invés "
+            "de vírgula)\n: ",
+            i + 1);
+        fgets(buf, sizeof(buf), stdin);
+        trim(buf);
+        if (!convert_float(buf, &lados[i])) {
+          puts("Valor não pode ser convertido em um número real, por favor "
+               "digite novamente");
+          continue;
+        }
+        printf("Gostaria de confirmar o número digitado %.2f? ", lados[i]);
+        fgets(buf, sizeof(buf), stdin);
+        trim(buf);
+        if (strcmp(buf, "s") == 0 || strcmp(buf, "S") == 0) {
+          wait(750);
+          printf("Ok, número %.2f confirmado!\n", lados[i]);
+          break;
+        } else {
+          puts("Resposta negativa ou inválida digitada, por favor, digite "
+               "novamente.");
+          continue;
+        }
+      }
+    }
+    result = check_triangle(&lados[0], &lados[1], &lados[2]);
+    switch (result) {
+    case 0:
+      puts("Não é possível formar um triângulo com esses lados. Tente "
+           "novamente.");
+      continue;
+    case 1:
+      puts("Erro interno: ponteiro inválido.");
+      return 1;
+    case 2:
+      puts("Erro: algum lado é inválido (<= 0).");
+      continue;
+    case 3:
+      if (lados[0] == lados[1] && lados[1] == lados[2]) {
+        puts("Triângulo equilátero!");
+      } else if (lados[0] == lados[1] || lados[0] == lados[2] ||
+                 lados[1] == lados[2]) {
+        puts("Triângulo isósceles!");
+      } else {
+        puts("Triângulo escaleno!");
+      }
+      break;
+    }
+  }
+  return 0;
 }
 
 bool convert_float(char *in, float *out) {
@@ -48,3 +111,23 @@ void trim(char *buf) {
 }
 
 void wait(unsigned short ms) { usleep(ms * 1000); }
+
+int check_triangle(float *a, float *b, float *c) {
+  if (!a || !b || !c) {
+    return 1;
+  }
+  // dereferencio os ponteiros, obtendo os valores do endereço de memória(&var)
+  float x = *a;
+  float y = *b;
+  float z = *c;
+
+  if (x <= 0 || y <= 0 || z <= 0) {
+    return 2;
+  }
+
+  if ((x + y > z) && (x + z > y) && (y + z > x)) {
+    return 3;
+  }
+
+  return 0;
+}
