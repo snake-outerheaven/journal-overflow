0d311e1a9c977f5d6cce1125bed56726eb504208
neo-dev <sei133dirsoa@gmail.com>
2025-12-10 23:15:18 -0300

    Create guessing game exercise.

--- ch01/ch01-ex1_1.c
+++ ch01/ch01-ex1_1.c
@@ -8,65 +8,37 @@
         Protótipos de funções.
 */
 
-int clear(void);
-int stripe(char *str);
-
-// int main(void) {}
-
-int clear(void)
+int main(void)
 {
-        return system("cls") ? 0 : 1;
-}
+        int secret, guess, tries = 0;
 
-int stripe(char *str)
-{
-        size_t end = strlen(str);
-        if (end == 0)
-                return 2;
-        end--; // \0
-        size_t start = 0;
-        while (start <= end && isspace((unsigned char)str[start]))
-                start++;
-        while (end >= start && isspace((unsigned char)str[end]))
-                end--;
-        memmove(str, str + start, end - start + 1);
-        str[end-start+1] = '\0';
-        return 0;
-}
-// [H][e][l][l][o][][][][][][\0]
-int main()
-{
-
-        int numero_aleatorio_da_silva;
-        int chute;
-        int tentativas = 0;
-
-        printf("Duvido tu adivinhar meu numero kkkk \n");
-        printf("é um número de 1 a 100 \n ");
-
-        do
+        srand((unsigned int)time(NULL));
+        secret = rand() % 100 + 1;
+        while (1)
         {
-                printf("faça um chute: ");
-                scanf("%d", &tentativas);
-
-                tentativas++; // tentativa = tentativa + 1;
-
-                if (tentativas < numero_aleatorio_da_silva)
+                printf("Write your guess: ");
+                if (scanf("%3d", &guess) != 1)
                 {
-                        printf("Tá frio! Tenta um numero maior.\n");
+                        guess = 0;
+                        printf("Wrong input.\n");
+                        return EXIT_FAILURE;
                 }
-                else if (tentativas > numero_aleatorio_da_silva)
+                tries++;
+                if (secret < guess)
                 {
-                        printf("Tá quente! Tenta um numero menor.\n");
+                        printf("Too high!\n");
+                        continue;
+                }
+                else if (secret > guess)
+                {
+                        printf("Too low!\n");
+                        continue;
                 }
                 else
                 {
-                        printf("Parabéns, Você adivinhou o número aleatorio da silva\n");
-                        printf("O Número era %d\n", numero_aleatorio_da_silva);
-                        printf("tu gastou %d tentativas.\n", tentativas);
+                        printf("Congrats, you done it in %d tries!\n", tries);
+                        return EXIT_SUCCESS;
                 }
-
-        } while (tentativas != numero_aleatorio_da_silva);
-
-        return 0;
+        }
 }
+
