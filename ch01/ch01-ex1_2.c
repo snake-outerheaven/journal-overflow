87beb64599a076bf251082f9719aa3fdb1bd9648
neo-dev <sei133dirsoa@gmail.com>
2025-12-12 11:00:51 -0300

    Create ch01-ex1_2.c

--- ch01/ch01-ex1_2.c
+++ ch01/ch01-ex1_2.c
@@ -1,5 +1,24 @@
-#include <stdio.h>
+#include <stdio.h> // incluo informações da biblioteca padrão de entrada e saída de C
 
 /*
-        Exercise that shows 
+        print Fahrenheit-Celsius table
+        for fahr = 0, 20, ..., 300
 */
+
+int main() // inicio do programa sempre na função main.
+{
+        int fahr, celsius;
+        int lower, upper, step;
+
+        lower = 0; // limite inferior da tabela
+        upper = 300; // limite superior da tabela
+        step = 20; // tamanho do passo a cada iteração do loop abaixo
+
+        while (fahr <= upper)
+        {
+                celsius = 5 * (fahr - 32) / 9;
+                printf("%d\t->\t%d\n",fahr,celsius);
+                fahr = fahr + step; // fahr += step; significa a mesma coisa e fica mais fácil de escrever.
+        }
+        return 0; // fim, do programa
+}
