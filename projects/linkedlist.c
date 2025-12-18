c468de579431dea277bdb4896463e67ea962394b
neo-dev <sei133dirsoa@gmail.com>
2025-12-18 01:36:11 -0300

    Create little exercise on projects for a bit of dynamic memory allocation and a doubly linked list.

---
+++ projects/linkedlist.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node Node;
+
+struct node
+{
+        struct node *prev;
+        void *data;
+        struct node *next;
+};
+
+int main()
+{
+        Node *p, *n;
+
+        n = malloc(sizeof(Node));
+        p = malloc(sizeof(Node));
+
+        n->prev = NULL;
+        n->data = malloc(sizeof(char) * 60);
+        n->next = p;
+
+        p->prev = n;
+        p->data = malloc(sizeof(double) * 512);
+        p->next = NULL;
+
+        printf("Where main func is on memory: %p.\n", (void *)&main);
+        printf("Where p and n are on on memory: %p\t%p.\n", (void *)n, (void *)p);
+        printf("Where n data is on memory: %p.\n", (void *)n->data);
+        printf("Where it points: %p.\n", (void *)n->next);
+        printf("Where p data is on memory: %p.\n", (void *)p->data);
+        printf("Where it points: %p.\n", (void *)p->prev);
+        printf("Where n->prev points: %p.\n", (void *)n->prev);
+        printf("Where p->next points: %p.\n", (void *)p->next);
+
+        free(n->data);
+        free(p->data);
+        free(p);
+        free(n);
+
+        n->data = NULL;
+        p->data = NULL;
+        p = NULL;
+        n = NULL;
+        
+        return 0;
+}
