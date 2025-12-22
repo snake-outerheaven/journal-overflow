64d6be660fc3182fd92ca0926f119c3ed6fde284
neo-dev <sei133dirsoa@gmail.com>
2025-12-22 16:15:24 -0300

    Add exercises 1-11 and 1-12.

---
+++ ch01/ch01-ex1_10.c
@@ -0,0 +1,9 @@
+/*
+        How would you test the word count program? What kinds of input are most likely to uncover bugs if there are any?
+ */
+
+
+ /*
+        R: while this code doesn't has any flaws, at least to me on my current level of knowledge, its interesting because i tried many outputs, but all of them are
+            on ASCII table, i think going unicode could will probably break this program, but it isnt fair since the book is 1988.
+ */
---
+++ ch01/ch01-ex1_11.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/*
+        Write a program that prints its input one word per line.
+        this is exercise 1-12
+*/
+#define OUT 0
+#define IN 1
+
+int main()
+{
+        int c, state;
+
+        state = OUT;
+
+        while ((c = getchar()) != EOF)
+        {
+                if (c == ' ' || c == '\n' || c == '\t')
+                {
+                        /*Too bad i forgot this  very important part, where i needed to check if the program was actually
+                                checking if the FSM of in and out of words  was working correctly.*/
+                        if (state == IN)
+                                putchar('\n');
+                        state = OUT;
+                }
+                else
+                {
+                        state = IN;
+                        putchar(c);
+                }
+        }
+        return 0;
+}
