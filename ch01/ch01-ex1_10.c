c31f498a71f9c25ac70e5ca19d4a8b5b88925952
neo-dev <sei133dirsoa@gmail.com>
2025-12-24 14:18:24 -0300

    Correct answer to ex 1-10

--- ch01/ch01-ex1_10.c
+++ ch01/ch01-ex1_10.c
@@ -5,5 +5,5 @@
 
  /*
         R: while this code doesn't has any flaws, at least to me on my current level of knowledge, its interesting because i tried many outputs, but all of them are
-            on ASCII table, i think going unicode could will probably break this program, but it isnt fair since the book is 1988.
+            on ASCII table, i think going unicode could will probably break this program, but it isnt fair since the book is from 1988.
  */
