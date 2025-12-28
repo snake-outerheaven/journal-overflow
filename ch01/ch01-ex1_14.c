025252b87f4d7f31002120c146817131b54c490c
neo-dev <sei133dirsoa@gmail.com>
2025-12-27 23:47:03 -0300

    Finish exercise 1-14.

--- ch01/ch01-ex1_14.c
+++ ch01/ch01-ex1_14.c
@@ -2,7 +2,7 @@
 
 /*
         Write a program to print a histogram of the frequencies of different characters in its input.
-        This is exercise 1-14
+        Exercise 1-14 (K&R)
 */
 
 #define LETTERS 52
@@ -12,14 +12,14 @@ typedef enum state
         READ_STATE,
         CLASSIFY_STATE,
         EOF_STATE,
-        PRINT_STATE,
+        PRINT_STATE
 } State;
 
-int main()
+int main(void)
 {
         State state;
         int c;
-        int i;
+        int i, i2;
         int freq[LETTERS];
 
         state = READ_STATE;
@@ -33,7 +33,6 @@ int main()
                 {
                 case READ_STATE:
                         c = getc(stdin);
-
                         if (c == EOF)
                                 state = EOF_STATE;
                         else
@@ -41,20 +40,46 @@ int main()
                         break;
 
                 case CLASSIFY_STATE:
-                        if (c >= 'a' && c <= 'z')
-                                freq[c - 'a']++;
-                        else if (c >= 'A' && c <= 'Z')
-                                freq[c - 'A']++;
+                        if (c >= 'A' && c <= 'Z')
+                                freq[c - 'A']++; /* 0..25 */
+                        else if (c >= 'a' && c <= 'z')
+                                freq[c - 'a' + 26]++; /* 26..51 */
                         state = READ_STATE;
                         break;
 
                 case EOF_STATE:
                         state = PRINT_STATE;
                         break;
-
-                default:
-                        printf("Undefined behavior awaits here.\n");
-                        break;
                 }
         }
+
+        /* print uppercase */
+        for (i = 0; i < 26; i++)
+        {
+                printf("%c -> ", 'A' + i);
+                for (i2 = 0; i2 < freq[i]; i2++)
+                        printf("* ");
+                putchar('\n');
+        }
+
+        putchar('\n');
+
+        /* print lowercase */
+        for (i = 0; i < 26; i++)
+        {
+                printf("%c -> ", 'a' + i);
+                for (i2 = 0; i2 < freq[i + 26]; i2++)
+                        printf("* ");
+                putchar('\n');
+        }
+
+        return 0;
 }
+
+/*
+        Previous versions of this code were failing because i did something real stupid
+
+        i basically failed to realize the freq array had the uppercase base already defined
+        by default, so when i did  c - 'a' i was writing onto invalid memory and then after 
+        
+*/
