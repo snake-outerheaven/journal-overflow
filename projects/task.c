1ec016d01a153128a8fe0f67a1e5e271dc1557cc
neo-dev <sei133dirsoa@gmail.com>
2026-01-12 23:28:25 -0300

    Rewrite task.c and task.h, the linked list is not a good strucutre, instead, a simple dynamic array will do the trick, good thing of C is that is there is always a simpler solution.

--- projects/task.c
+++ projects/task.c
@@ -1,7 +1,5 @@
 #include "task.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 
 /*
         this file will hold the implementations of task.h
@@ -15,48 +13,3 @@ struct task
     char taskdescription[250];
 };
 
-struct tasknode
-{
-    struct task *task;
-    struct tasknode *prev;
-    struct tasknode *next;
-};
-
-struct tasklist
-{
-    struct tasknode *head;
-    struct tasknode *tail;
-    int size;
-    int capacity;
-};
-
-Task *task_create(const char *nm, const char *desc, const int done)
-{
-    static int id = 0;
-    Task *t;
-
-    t = (Task *)malloc(sizeof(Task)); /* explicit typecasting, why not? */
-
-    if (!t)
-    {
-        perror("malloc");
-        return NULL;
-    }
-
-    t->id = id++;
-    strncpy(t->taskname, nm, sizeof(t->taskname) - 1);
-    t->taskname[sizeof(t->taskname) - 1] = '\0';
-    strncpy(t->taskdescription, desc, sizeof(t->taskdescription) - 1);
-    t->taskdescription[sizeof(t->taskdescription) - 1] = '\0';
-    t->done = done;
-
-    return t;
-}
-
-void task_free(Task *t)
-{
-    if (t)
-        free(t);
-    else
-        fprintf(stderr, "Tried to free a null task.");
-}
--- projects/task.h
+++ projects/task.h
@@ -4,23 +4,4 @@
 /* This is essentialy a record. */
 typedef struct task Task;
 
-/* this is the node of the data structure*/
-typedef struct tasknode TaskNode;
-
-/* contains a pointer to the head, a pointer to the tail,
-a item counter and a capacity counter.*/
-typedef struct tasklist TaskList;
-
-Task *task_create(const char *, const char *, const int);
-void task_free(Task *);
-
-TaskList *create_tasklist(int);
-void free_tasklist(TaskList *);
-
-void tasklist_insert_head(TaskList *, Task *);
-void tasklist_insert_tail(TaskList *, Task *);
-void tasklist_delete_head(TaskList *, Task *);
-void tasklist_delete_tail(TaskList *);
-void tasklist_print_tasks(const TaskList *);
-
 #endif
