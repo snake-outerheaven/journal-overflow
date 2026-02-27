db1666a1b5213323e61c2a0bc225d82f6fcfea62
neo-dev <sei133dirsoa@gmail.com>
2026-02-27 20:53:07 -0300

    Implement getters and setters for task in task_manager

--- projects/task_manager/task.c
+++ projects/task_manager/task.c
@@ -47,3 +47,48 @@ void task_delete(task_t **t)
 
 	*t = NULL;
 }
+
+
+int task_get_id(const task_t *t, int *out)
+{
+	if(!t || !out)
+		return 1;
+
+	*out = t->id;
+
+	return 0;
+}
+
+int task_set_id(task_t *t, int in)
+{
+	if(!t)
+		return 1;
+
+	t->id = in;
+
+	return 0;
+}
+
+int task_get_taskname(const task_t *t, char *out)
+{
+	if(!t || !out)
+		return 1;
+
+	strncpy(out, t->taskname, MAXBUF - 1);
+
+	out[MAXBUF - 1] = '\0';
+
+	return 0;
+}
+
+int task_set_taskname(task_t *t, char *out)
+{
+	if(!t || !out)
+		return 1;
+
+	strncpy(t->taskname, out, MAXBUF - 1);
+
+	t->taskname[MAXBUF - 1] = '\0';
+
+	return 0;
+}
--- projects/task_manager/task.h
+++ projects/task_manager/task.h
@@ -7,7 +7,20 @@ task_t *task_init(void);
 void task_delete(task_t **);
 
 /*
- * TODO:Create getter and setters for tasks
+ * TODO:Finish the rest of the getters and setters for the task struct
+ * 	on the task.c file.
  */
+
+int task_get_id(const task_t *, int *);
+int task_set_id(task_t *, int);
+
+int task_get_taskname(const task_t *, char *);
+int task_set_taskname(task_t *, char *);
+
+int task_get_desc(const task_t *, char *);
+int task_set_desc(task_t *, char *);
+
+int task_get_done(const task_t *, int *);
+int task_set_done(task_t *, int);
 
 #endif
