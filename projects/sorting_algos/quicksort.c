244bf8777423a91a81b0704655de76d5525f70ea
neo-dev <sei133dirsoa@gmail.com>
2026-03-17 22:47:41 -0300

    Change variables name on quicksort project for a more readable source code.

--- projects/sorting_algos/quicksort.c
+++ projects/sorting_algos/quicksort.c
@@ -3,75 +3,70 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX 20
+#define MAX 90
 
 void skip()
 {
-	putchar('\n');
-	putchar('\n');
+    putchar('\n');
+    putchar('\n');
 }
 
 void quicksort(int *item, int left, int right)
 {
-	int i, j;
-	int x, y;
+    int walker1, walker2;
+    int pivot, temp;
 
-	i = left, j = right;
-	x = item[(right + left) / 2];
+    walker1 = left, walker2 = right;
+    pivot = item[(right + left) / 2];
 
-	do
-	{
-		while(x > item[i] && i < right)
-			i++;
+    do
+    {
+        while (pivot > item[walker1] && walker1 < right)
+            walker1++;
 
-		while(x < item[j] && j > left)
-			j--;
+        while (pivot < item[walker2] && walker2 > left)
+            walker2--;
 
-		if(i <= j)
-		{
-			y = item[i];
-			item[i] = item[j];
-			item[j] = y;
-			i++, j--;
-		}
-	} while(i <= j);
+        if (walker1 <= walker2)
+        {
+            temp = item[walker1];
+            item[walker1] = item[walker2];
+            item[walker2] = temp;
+            walker1++, walker2--;
+        }
+    } while (walker1 <= walker2);
 
-	if (left < j)
-		quicksort(item, left, j);
-	if (right > i)
-		quicksort(item, i, right);
-
+    if (left < walker2)
+        quicksort(item, left, walker2);
+    if (right > walker1)
+        quicksort(item, walker1, right);
 }
 
 void quick(int *item, int count)
 {
-	quicksort(item, 0, count - 1);
+    quicksort(item, 0, count - 1);
 }
 
 int main()
 {
-	int i;
-	int array[MAX];
+    int i;
+    int array[MAX];
 
-	srand(time(NULL));
+    srand(time(NULL));
 
-	for(i = 0; i < MAX; i++)
-	{
-		array[i] = rand() % MAX;
-	}
+    for (i = 0; i < MAX; i++)
+        array[i] = rand() % MAX;
 
+    for (i = 0; i < MAX; i++)
+        printf("|\t%d\t|\n", array[i]);
 
-	for(i = 0; i < MAX; i++)
-		printf("|\t%d\t|\n", array[i]);
+    quick(array, MAX);
 
-	quick(array, MAX);
+    for (i = 0; i < 2; i++)
+        skip();
 
-	for(i = 0; i < 2; i++)
-		skip();
+    for (i = 0; i < MAX; i++)
+        printf("|\t%d\t|\n", array[i]);
 
-
-	for(i = 0; i < MAX; i++)
-		printf("|\t%d\t|\n", array[i]);
-
-	return 0;
+    return 0;
 }
