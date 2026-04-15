a0c70089dd28d7ff24f1d01f61f59d989e79d4b6
neo-dev <sei133dirsoa@gmail.com>
2026-04-14 23:08:31 -0300

    Refactor map to a fixed size.

--- projects/map/map.c
+++ projects/map/map.c
@@ -5,76 +5,55 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_CAPACITY 10
+
 typedef struct node_t
 {
     char key[MAXBUF];
     char value[MAXBUF];
-    node_t *next;
+    struct node_t *next;
 } node_t;
 
 struct map_t
 {
-    node_t **mapped;
+    node_t *mapped[MAX_CAPACITY];
     size_t size;
     size_t capacity;
 };
 
-map_t *map_init(size_t size)
+map_t *map_init(void)
 {
-    // calloc already initializes size and capacity to 0,
-    // this behaviour is mostly cross platform
-    map_t *map = malloc(sizeof(map_t));
+    map_t *map = calloc(1, sizeof(map_t));
 
     if (!map)
         return NULL;
 
-    map->mapped =
-        calloc(INIT_CAPACITY,
-               sizeof(*map->mapped)); // inicializa matriz com ponteiros NULL.
-
-    if (!map->mapped)
-    {
-        free(map);
-        return NULL;
-    }
-
     map->size = 0;
-    map->capacity = INIT_CAPACITY;
+    map->capacity = MAX_CAPACITY;
 
     return map;
 }
 
 int map_kill(map_t **map)
 {
-    size_t i;
-    map_t *temp;
-    node_t *curr;
-    node_t *next;
-
-    if (!map)
+    if (!map || !*map)
         return 1;
 
-    if (!*map)
-        return 2;
+    map_t *temp = *map;
 
-    temp = *map;
-
-    for (i = 0; i < temp->capacity; ++i)
+    for (size_t i = 0; i < MAX_CAPACITY; ++i)
     {
-        curr = temp->mapped[i];
+        node_t *curr = temp->mapped[i];
 
         while (curr)
         {
-            next = curr->next;
+            node_t *next = curr->next;
             free(curr);
             curr = next;
         }
     }
 
-    free(temp->mapped);
-
     free(temp);
-
     *map = NULL;
 
     return 0;
