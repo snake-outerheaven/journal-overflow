1a68c799d57a43498b6a37f1dcf00765ec61a8b4
neo-dev <sei133dirsoa@gmail.com>
2026-05-06 14:32:10 -0300

    Add for loop on N1_V2

--- tasks/N1/N1_V2.c
+++ tasks/N1/N1_V2.c
@@ -7,12 +7,13 @@
 #include <string.h>
 #include <time.h>
 
-#define N 123
+#define N 123     // numero de notas
+#define MAXBUF 20 // tamanho de strings
 
-#define MAXDAY 30
+#define MAXDAY 30 // max de dias
 
-#define NUM_MIN 1550
-#define NUM_MAX 1800
+#define NUM_MIN 1550 // número mínimo para compor o código da nota
+#define NUM_MAX 1800 // número máximo para compor o código da nota
 
 int main(void)
 {
@@ -23,7 +24,7 @@ int main(void)
 
     // valores individuais de cada nota.
     int ts[N], loc[N], val[N], day[N];
-    char code[N][20], mes[N][20];
+    char code[N][MAXBUF], mes[N][MAXBUF];
 
     // variáveis para salvar a maior nota
     char codeMaiorV[20], mesMaiorV[20];
@@ -46,8 +47,77 @@ int main(void)
     // variáveis para armazenar o percentual de notas especial + média de dias.
     float perNtLoc115, avgdayTot, avgDayTs1, avgDayTs0;
 
-    setlocale(LC_ALL, "pt_BR.UTF-8"); // seta o terminal para português brasileiro UTF-8
+    setlocale(LC_ALL,
+              "pt_BR.UTF-8"); // seta o terminal para português brasileiro UTF-8
     srand(time(NULL));
+
+    num = rand() % (NUM_MAX - NUM_MIN + 1) + NUM_MIN;
+
+    for (i = 0; i < N; ++i)
+    {
+        ts[i] = rand() % 2;
+        loc[i] = rand() % 2;
+        day[i] = rand() % MAXDAY + 1;
+
+        if (ts[i])
+        {
+            val[i] = day[i] * 450 + 750;
+            sumTs1 += day[i];
+            contTs1++;
+        }
+
+        else
+        {
+            val[i] = day[i] * 300 + 500;
+            sumTs0 += day[i];
+            contTs0++;
+        }
+
+        sumTsTot += day[i];
+
+        if (loc[i])
+        {
+            totLoc1++;
+
+            if (day[i] < 15)
+                totLoc115++;
+        }
+
+        if (i < 39)
+        {
+            sprintf(mes[i], "Setembro");
+            val39 += val[i];
+        }
+        else if (i < 80)
+        {
+            sprintf(mes[i], "Outubro");
+            val41 += val[i];
+        }
+        else
+        {
+            sprintf(mes[i], "Novembro");
+            val43 += val[i];
+        }
+
+        sprintf(code[i], "NT%d%c", num, ts ? 'T' : 'F');
+
+        if (val[i] > valCmp)
+        {
+            valCmp = val[i];
+            strcpy(codeMaiorV, code[i]);
+            strcpy(mesMaiorV, mes[i]);
+            tsMaiorV = ts[i];
+            locMaiorV = loc[i];
+            dayMaiorV = day[i];
+            valMaiorV = val[i];
+        }
+      
+        printf("%-10s %-10s %-12s %-13s %5d %8d\n", code[i], mes[i],
+               ts[i] ? "Transporte" : "Fretamento",
+               loc[i] ? "Interestadual" : "Estadual", day[i], val[i]);
+
+        num++;
+    }
 
     return 0;
 }
