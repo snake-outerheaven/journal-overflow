eb543c403d6e9fc489b2e7899cf6820b26f88309
neo-dev <sei133dirsoa@gmail.com>
2026-05-11 13:58:27 -0300

    Add ex10

---
+++ ch01/ex10.c
@@ -0,0 +1,28 @@
+/*
+ * Write a program which replaces tabs for \t, backspaces for \b and
+ * backslashes as \\
+ */
+
+#include <stdio.h>
+
+int
+main (void)
+{
+  int c;
+
+  while ((c = getchar ()) != EOF)
+    {
+      if (c == '\t')
+        printf ("\\t");
+      else if (c == '\b')
+        printf ("\\b");
+      else if (c == '\\')
+        printf ("\\\\");
+      else
+	putchar(c);
+    }
+
+  puts("EOF pressed, end of program.");
+  
+  return 0;
+}
