cb1ae7a64dee1899c9bb51a99c7f81c43cdb656d
neo-dev <sei133dirsoa@gmail.com>
2026-05-11 22:52:56 -0300

    Add ex11 answer.

---
+++ ch01/ex11.c
@@ -0,0 +1,17 @@
+/*
+ * How would you test the word count program? What kinds of input are most
+ * likely to uncover bugs if there are any?
+ */
+
+/*
+ *
+ * Answer: it doesn't cover Windows and DOS environments where RET is \r\n, it
+ * also doesn't cover multibyte characters as getchar reads the value, casts it
+ * to unsigned char (range 0-255) which only cover 1 byte characters, as ASCII
+ *
+ * the program also have a issue where it only counts lines when it meets '\n'
+ *
+ * it also has a issue in that special signs (!#?:$%) are treated as parts of 
+ * words.
+ */
+
