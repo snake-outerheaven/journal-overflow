23884abd93e18247999fdcb5a8ecfacf5eca984c
neo-dev <sei133dirsoa@gmail.com>
2026-06-05 17:03:08 -0300

    Add ex20 beginning.

---
+++ ch01/ex20.c
@@ -0,0 +1,14 @@
+/*
+   Write a program detab that replaces tabs in the input with the proper
+   number of blanks to space to the next tab stop. Assume a fixed set of tab
+   stops, say every n columns. Should n be a variable or a symbolic parameter?
+*/
+
+#include <stdio.h>
+
+#define TABSIZE 4
+
+#define IN 1
+#define OUT 0
+
+/* continue later */
