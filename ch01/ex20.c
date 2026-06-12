0f117e38340ddac79e26a38332dea1748e63d367
neo-dev <sei133dirsoa@gmail.com>
2026-06-11 23:06:46 -0300

    add ex20 finished

--- ch01/ex20.c
+++ ch01/ex20.c
@@ -1,16 +1,75 @@
-/*
-   Write a program detab that replaces tabs in the input with the proper
-   number of blanks to space to the next tab stop. Assume a fixed set of tab
-   stops, say every n columns. Should n be a variable or a symbolic parameter?
-*/
-
 #include <stdio.h>
 
-#define MAXLEN 300
+#define MAXLEN 256
 #define TABSIZE 4
 
-#define IN 1
-#define OUT 0
+int get_line (char line[], int max);
+int detab (char line[], int siz);
 
-int get_line (char[], int);
-int detab (char[]);
+int
+main (void)
+{
+  char line[MAXLEN];
+
+  while (get_line (line, MAXLEN) > 0)
+    {
+      detab (line, MAXLEN);
+    }
+
+  return 0;
+}
+
+int
+get_line (char line[], int max)
+{
+  int c, i;
+
+  i = 0;
+  while (i < max - 1 && (c = getchar ()) != EOF && c != '\n')
+    {
+      line[i] = c;
+      i++;
+    }
+  if (c == '\n')
+    {
+      line[i] = c;
+      i++;
+    }
+  line[i] = '\0';
+  return i;
+}
+
+int
+detab (char line[], int siz)
+{
+  int i, x;
+  int col;
+  int spaces;
+
+  col = 0;
+  for (i = 0; i < siz && line[i] != '\0'; i++)
+    {
+      if (line[i] == '\t')
+        {
+          spaces = TABSIZE - (col % TABSIZE);
+          for (x = 0; x < spaces; x++)
+            {
+              putchar (' ');
+              col++;
+            }
+        }
+      else
+        {
+          putchar (line[i]);
+          if (line[i] == '\n')
+            {
+              col = 0;
+            }
+          else
+            {
+              col++;
+            }
+        }
+    }
+  return 0;
+}
