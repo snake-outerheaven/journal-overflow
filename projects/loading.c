d20e3935b6c0a4f793b938ff40b5b9e0ee7fd998
neo-dev <sei133dirsoa@gmail.com>
2026-08-03 19:13:35 -0300

    change animation time on Sleep function.

--- projects/loading.c
+++ projects/loading.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <windows.h>
 
-int main(void) {
-    char spinner[] = { '/', '|', '\\', '-' };
-    int i = 0;
+int
+main (void)
+{
+  char spinner[] = { '/', '|', '\\', '-' };
+  int i = 0;
 
-    for (int count = 0; count < 20; count++) {
-        printf("\rLoading... %c", spinner[i]);
-        fflush(stdout);
-        Sleep(500); 
-        i = (i + 1) % 4;
+  for (int count = 0; count < 20; count++)
+    {
+      printf ("\rLoading... %c", spinner[i]);
+      fflush (stdout);
+      Sleep (250);
+      i = (i + 1) % 4;
     }
 
-    printf("\rDone!        \n");
-    return 0;
+  printf ("\rDone!        \n");
+  return 0;
 }
