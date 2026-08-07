1b2611e8cc699a452aa9ec7bc8c97e1478e15d74
neo-dev <sei133dirsoa@gmail.com>
2026-08-07 08:32:23 -0300

    feat(game): improve game loop and input validation

--- tasks/Prog2/class-exercises/game.c
+++ tasks/Prog2/class-exercises/game.c
@@ -1,47 +1,54 @@
 #include <locale.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define MAX 100
 #define MIN 1
-#define MAX_TRIES 40
+#define MAX_TRIES 99
+#define ARR_SIZE(buf) (sizeof (buf) / sizeof (buf[0]))
 
 int
 main (void)
 {
-  int num, i, input;
-
+  char buf[64], *YES[] = { "s", "sim", "y", "yes" };
+  int i, x, num, input;
   srand (time (0));
-  setlocale (LC_ALL, "pt_BR");
+  setlocale (LC_ALL, "pt_BR.UTF-8");
 
-  num = rand () % (MAX - MIN + 1) + MIN;
-
-  for (i = 0; i < MAX_TRIES; i++)
+  while (1)
     {
-      printf ("Por favor, digite um número entre %d e %d, você tem %d "
-              "tentativas: ",
-              MIN, MAX, MAX_TRIES - i);
-      scanf ("%d", &input);
-      if (input == num)
+      num = rand () % (MAX - MIN + 1) + MIN;
+      for (i = 0; i < MAX_TRIES; ++i)
         {
-          printf ("Parabens, você acertou o número com %d tentativas!\n",
-                  i + 1);
-          return 0;
+          printf ("Digite um número entre %d e %d (%d tentativas restantes): ",
+                  MIN, MAX, MAX_TRIES - i);
+          if (!fgets (buf, sizeof buf, stdin))
+            return 0;
+          if (sscanf (buf, "%d", &input) != 1)
+            {
+              printf ("Entrada inválida.\n");
+              continue;
+            }
+          if (input == num)
+            {
+              printf ("Parabéns — acertou em %d tentativas!\n", i + 1);
+              break;
+            }
+          printf ("O número é %s que %d!\n", input > num ? "menor" : "maior",
+                  input);
         }
-      else if (input > num)
-        {
-          printf ("O número secreto é menor que %d!\n", input);
-          continue;
-        }
-      else if (input < num)
-        {
-          printf ("O número secreto é maior que %d!\n", input);
-          continue;
-        }
+      printf ("Jogar novamente? (s/n): ");
+
+      if (!fgets (buf, sizeof buf, stdin))
+        break;
+
+      for (x = 0; x < ARR_SIZE (YES); ++x)
+        if (strcmp (buf, YES[x]) == 0)
+          break;
     }
-  if (input != num)
-    printf ("O número era %d! Mais sorte na próxima vez!\n", num), main ();
 
   return 0;
 }
